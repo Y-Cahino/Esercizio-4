f25d368c7c8723ff596346ca474cf5205d7efbe0
cahinosilva.20184 <cahinosilva.20184@LABI2-13PC05>
2023-03-06 13:22:40 +0100

    Aggiungere i file di progetto.

---
+++ Esercizio 4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Esercizio_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inserire primo numero");
+            int n1=int.Parse(Console.ReadLine());
+            Console.WriteLine("Inserire secondo numero");
+            int n2=int.Parse(Console.ReadLine());
+            calcolamcd(n1,n2);
+        }
+        static int calcolamcd(int n1,int n2)
+
+        {
+            while(n2!=0)
+                {
+                int temp = n2;
+                n2 = n1 % n2;
+                n1 = temp;
+            }
+            return 0;
+        }
+    }
+}
